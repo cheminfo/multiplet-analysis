5c8141e4f36443037e07cc91a44c2929e0ae0c52
Damien Jeannerat <damien.jeannerat@unige.ch>
2020-01-13 16:18:23 +0100

    add loop over J* and test resolution

--- src/__tests__/index.test.js
+++ src/__tests__/index.test.js
@@ -4,7 +4,7 @@ import quadruplet from './data/d=1_J=7_m=q.json';
 import { analyseSignal } from '..';
 
 describe('analyseSignal of simulated spectra', () => {
-  it('d=2_J=7_m=d', () => {
+  it('d=2_J=7_m=d', () => {// was it.only
     let result = analyseSignal(doublet, { frequency: 400 });
     expect(result.multiplicity).toBe('');
     expect(result.j[0]).toStrictEqual({ multiplicity: 'd', coupling: 7 });
--- src/index.js
+++ src/index.js
@@ -7,17 +7,40 @@
 export function analyseSignal(data = {}, options = {}) {
   const { x = [], y = [] } = data;
   const { frequency = 400 } = options;
-
+  const { debug = true } = options;
+  const { maxTestedJ = 20 } = options;
+  const { minTestedJ = 0.5 } = options;
+  const { minimalResolution = 0.01 } = options;
+  var jStar = new Array();
   let result = {};
 
-  console.log('I want to debug this');
+  // determine if need interpolation
+  let resolutionPpm = Math.abs(x[0]-y[x.length-1]) / x.length;
+  let resolutionHz = resolutionPpm * frequency;
+  // test if interpolation is needed. 
+  // if (resolutionHz < minimalResolution) {}
+  // if yes interpolate and update x and y
 
+  // recalculate resolution after interpolation
+   resolutionPpm = Math.abs(x[0]-y[x.length-1]) / x.length;
+   resolutionHz = resolutionPpm * frequency;
+   let minTestedPt = Math.trunc(maxTestedJ * resolutionHz);
+   let maxTestedPt = Math.trunc(minTestedJ * resolutionHz);
+  for (let index = maxTestedPt; index >= minTestedPt; index -= 1) {
+    result[index] = 4;
+  }
+ if (debug) {
+  console.log(resolutionHz + " Hz per point");
+  console.log("min " + minTestedPt + " in pt");
+  console.log("max " + maxTestedPt + " in pt");
+}
+  
   // we do some complex stuff ...
   result.delta = 7.2;
   result.multiplicity = '';
   result.j = [];
-  result.j.push({ multiplicy: 'd', coupling: 7 });
-  result.j.push({ multiplicy: 't', coupling: 2 });
+  result.j.push({ multiplicity: 'd', coupling: 7 });
+  result.j.push({ multiplicity: 't', coupling: 2 });
 
   return result;
 }
