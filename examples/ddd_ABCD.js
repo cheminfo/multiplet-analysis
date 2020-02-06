3eee4cc8a025a8ed9bf35a860b25a6c869fbe7e9
Luc Patiny <luc@patiny.com>
2020-02-06 18:36:17 +0100

    feat: add ddd_ABCD example

---
+++ examples/ddd_ABCD.js
@@ -0,0 +1,16 @@
+import ddd from '../data/d=1_J=2,4,6_m=ddd_ABCD.json';
+import { analyseMultiplet } from '../src/index';
+import { writeFileSync } from 'fs';
+import { join } from 'path';
+
+let result = analyseMultiplet(ddd, {
+  frequency: 400,
+  debug: true,
+  symmetrizeEachStep: true,
+});
+
+writeFileSync(
+  join(__dirname, 'web', 'result.json'),
+  JSON.stringify(result, null, 2),
+  'utf8',
+);
