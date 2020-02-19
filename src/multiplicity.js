9e2445776ad59c3772dbb7d8e7a1bf71948807da
Damien Jeannerat <damien.jeannerat@unige.ch>
2020-02-19 22:19:37 +0100

    fixed variable name

--- src/multiplicity.js
+++ src/multiplicity.js
@@ -1,9 +1,9 @@
 export function getGeneralPascal(n, spin = 0.5) {
   //spin = typeof spin !== 'undefined' ? spin : 0.5;
   if (n === 0) return [1];
-  const mult = 2 * spin + 1;
+  const nbLine = 2 * spin + 1;
   let line;
-  if (mult === 2) {
+  if (nbLine === 2) {
     // this algorithm is faster for spin 1/2 from https://gist.github.com/lpatiny/5956d1866998fc565d4c676cf4a2be3b
     line = [1];
     for (let i = 0; i < n - 0; i++) {
@@ -14,12 +14,12 @@ export function getGeneralPascal(n, spin = 0.5) {
     // this is a more general algorithm
     line = [1];
     if (n === 0) return line;
-    for (let j = 0; j < mult - 1; j++) line.push(1);
+    for (let j = 0; j < nbLine - 1; j++) line.push(1);
     // complete with "1 1" or "1 1 1" for spin 1/2 and 1 respectively
     let previousLine = line;
     for (let i = 0; i < n - 1; i++) {
       line = [];
-      for (let j = 0; j < mult; j++) {
+      for (let j = 0; j < nbLine; j++) {
         if (j === 0) {
           for (let k = 0; k < previousLine.length; k++) {
             line.push(previousLine[k]); // copy the line
