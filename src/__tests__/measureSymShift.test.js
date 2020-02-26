234eb23b2077cdda885bbd1f3b27395f85ca22a3
Damien Jeannerat <damien.jeannerat@unige.ch>
2020-02-26 12:01:20 +0100

    unsaved change

--- src/__tests__/measureSymShift.test.js
+++ src/__tests__/measureSymShift.test.js
@@ -6,6 +6,8 @@ describe('analyse multiplet of simulated spectra', () => {
     expect(measureSymShift([0, 1, 0, 1, 0])).toStrictEqual(0);
     expect(measureSymShift([0, 0, 1, 0, 1, 0])).toStrictEqual(1);
     expect(measureSymShift([0, 0, 1, 0, 1, 0, 0, 0, 0])).toStrictEqual(-2);
-    expect(measureSymShift([0, 0, 0, 0, 1, 0, 1.2, 0, 0, 0, 0])).toStrictEqual(0);
+    expect(measureSymShift([0, 0, 0, 0, 1, 0, 1.2, 0, 0, 0, 0])).toStrictEqual(
+      0,
+    );
   });
 });
