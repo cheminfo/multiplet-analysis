a173a7996a8ffede72abc1a44ba67a50607dc12d
Luc Patiny <luc@patiny.com>
2020-07-18 13:39:14 +0200

    chore: improve speed of deco.js

--- src/__tests__/measureDeco.test.js
+++ src/__tests__/measureDeco.test.js
@@ -3,8 +3,14 @@ import { measureDeco } from '../measureDeco';
 describe('test measureDeco', () => {
   it('first should be', () => {
     //  it.only
-    expect(measureDeco([0, 1, 0, 1, 0], 2, 1, 1, 0.5, 1)).toStrictEqual(1);
-    expect(measureDeco([0, 1, 0, 1, 0], 3, 1, 1, 0.5, 1)).toStrictEqual(0);
-    expect(measureDeco([0, 1, 0, -1, 0], 2, -1, 1, 0.5, 1)).toStrictEqual(1);
+    expect(
+      measureDeco(Float64Array.from([0, 1, 0, 1, 0]), 2, 1, 1, 0.5, 1),
+    ).toStrictEqual(1);
+    expect(
+      measureDeco(Float64Array.from([0, 1, 0, 1, 0]), 3, 1, 1, 0.5, 1),
+    ).toStrictEqual(0);
+    expect(
+      measureDeco(Float64Array.from([0, 1, 0, -1, 0]), 2, -1, 1, 0.5, 1),
+    ).toStrictEqual(1);
   });
 });
--- src/deco.js
+++ src/deco.js
@@ -16,41 +16,64 @@ export function deco(
   multiplicity = 0.5,
 ) {
   let nbLines = parseInt(2 * multiplicity, 10); // 1 for doublet (spin 1/2) 2, for spin 1, etc... never tested...
-  let y1 = new Float64Array(yi.length);
-  let y2 = new Float64Array(yi.length);
+  let y1;
+  let y2;
   if (dir > -1) {
     y1 = yi.slice();
-    for (let scan = 0; scan < y1.length - jStar * nbLines; scan++) {
-      for (let line = 0; line < nbLines; line++) {
-        y1[scan + (line + 1) * jStar] -= sign * y1[scan];
+    if (nbLines === 1) {
+      for (let scan = 0; scan < y1.length - jStar * nbLines; scan++) {
+        y1[scan + jStar] -= sign * y1[scan];
+      }
+    } else {
+      for (let scan = 0; scan < y1.length - jStar * nbLines; scan++) {
+        for (let line = 0; line < nbLines; line++) {
+          y1[scan + (line + 1) * jStar] -= sign * y1[scan];
+        }
       }
     }
     if (dir > 0.5) {
-      return y1.slice(0, y1.length - chopTail * jStar * nbLines);
+      return new Float64Array(
+        y1.buffer,
+        0,
+        y1.length - chopTail * jStar * nbLines,
+      );
     }
   }
   if (dir < 1) {
     y2 = yi.slice();
-    for (let scan = y2.length - 1; scan >= jStar * nbLines; scan -= 1) {
-      for (let line = 0; line < nbLines; line++) {
-        y2[scan - (line + 1) * jStar] -= sign * y2[scan];
+    if (nbLines === 1) {
+      for (let scan = y2.length - 1; scan >= jStar * nbLines; scan -= 1) {
+        y2[scan - jStar] -= sign * y2[scan];
+      }
+    } else {
+      for (let scan = y2.length - 1; scan >= jStar * nbLines; scan -= 1) {
+        for (let line = 0; line < nbLines; line++) {
+          y2[scan - (line + 1) * jStar] -= sign * y2[scan];
+        }
       }
     }
+
     if (dir < -0.2) {
-      return y2.slice(chopTail * jStar * nbLines, y2.length);
+      return new Float64Array(
+        y2.buffer,
+        chopTail * jStar * nbLines * 8,
+        y2.length - chopTail * jStar * nbLines,
+      );
     }
   }
+  if (!y1) y1 = new Float64Array(yi.length);
+  if (!y2) y2 = new Float64Array(yi.length);
   if (dir === 0) {
     for (let scan = 0; scan < y2.length - jStar * nbLines; scan++) {
       y1[scan] = (y1[scan] + sign * y2[scan + jStar * nbLines]) / 2;
     }
-    return y1.slice(0, y1.length - jStar * nbLines);
+    return new Float64Array(y1.buffer, 0, y1.length - jStar * nbLines);
   }
   let half = ((y1.length - jStar * nbLines) / 2.0) | 0;
   if (dir === 0.1) {
     for (let scan = half; scan < y2.length - jStar * nbLines; scan++) {
       y1[scan] = sign * y2[scan + jStar * nbLines];
     }
-    return y1.slice(0, y1.length - jStar * nbLines);
+    return new Float64Array(y1.buffer, 0, y1.length - jStar * nbLines);
   }
 }
