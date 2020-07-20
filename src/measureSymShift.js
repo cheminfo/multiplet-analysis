ff733f8761f3961085f3c7200655b88255b772cf
Damien Jeannerat <damien.jeannerat@unige.ch>
2020-07-20 11:17:06 +0200

    chore: Faster method for measure of symmetry - avoids creation of unnecessary arrays.

--- src/measureSymShift.js
+++ src/measureSymShift.js
@@ -11,21 +11,17 @@ export function measureSymShift(y) {
   scalarProductReference = scalarProduct(y, y, -1, 1);
   // search left...
   for (let i = 1; i < y.length / 2; i++) {
-    const tmpY = new Float64Array(y.buffer, i * 8, y.length - i);
-    scalarProductNewValue = scalarProduct(tmpY, tmpY, -1, 1);
+    scalarProductNewValue = scalarProduct(y, y, -1, 1, i, y.length);
     if (scalarProductNewValue > scalarProductReference) {
       scalarProductReference = scalarProductNewValue;
       movedBy = i;
     }
   }
-  if (movedBy === 0) {
-    for (let i = 1; i < y.length / 2; i++) {
-      const tmpY = new Float64Array(y.buffer, 0, y.length - i);
-      scalarProductNewValue = scalarProduct(tmpY, tmpY, -1, 1);
-      if (scalarProductNewValue > scalarProductReference) {
-        scalarProductReference = scalarProductNewValue;
-        movedBy = -i;
-      }
+  for (let i = 1; i < y.length / 2; i++) {
+    scalarProductNewValue = scalarProduct(y, y, -1, 1, 0, y.length - i);
+    if (scalarProductNewValue > scalarProductReference) {
+      scalarProductReference = scalarProductNewValue;
+      movedBy = -i;
     }
   }
   return movedBy;
--- src/scalarProduct.js
+++ src/scalarProduct.js
@@ -1,19 +1,26 @@
-export function scalarProduct(y1, y2, sens, incrementForSpeed) {
+export function scalarProduct(
+  y1,
+  y2,
+  sens,
+  incrementForSpeed,
+  first = 0,
+  last = y1.length,
+) {
   // sens = 1; crude scalar product
   // sens =-1: flip spectrum first
   let v11 = 0;
   let v22 = 0;
   let v12 = 0;
   if (sens > 0) {
-    for (let index = 0; index < y1.length; index += incrementForSpeed) {
+    for (let index = first; index < last; index += incrementForSpeed) {
       v12 += y1[index] * y2[index];
       v11 += y1[index] * y1[index];
       v22 += y2[index] * y2[index];
     }
   } else {
     // Here as if flip left/right array
-    for (let index = 0; index < y1.length; index += incrementForSpeed) {
-      v12 += y1[index] * y2[y1.length - index - 1];
+    for (let index = first; index < last; index += incrementForSpeed) {
+      v12 += y1[index] * y2[last - (index - first) - 1];
       v11 += y1[index] * y1[index];
       v22 += y2[index] * y2[index];
     }
