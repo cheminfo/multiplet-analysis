cda3b5f017d45b7e8207a167e320875723417730
jobo322 <jose.bolanos@correounivalle.edu.co>
2023-05-04 13:14:33 -0500

    chore: add massive test case

--- src/__tests__/index.test.js
+++ src/__tests__/index.test.js
@@ -7,6 +7,7 @@ import asymDoublet from '../../data/asymDoublet.json';
 import ddd from '../../data/d=1_J=2,4,6_m=ddd.json';
 import quadruplet from '../../data/d=1_J=7_m=q.json';
 import doublet from '../../data/d=2_J=7_m=d.json';
+import massive from '../../data/massive.json';
 import toDebbug from '../../data/multiplet-analisys-toDebbug.json';
 
 describe('analyse multiplet of simulated spectra', () => {
@@ -158,4 +159,13 @@ describe('analyse multiplet of simulated spectra', () => {
       1,
     );
   });
+
+  it('massive: checkSymmetryFirst option true', () => {
+    let result = analyseMultiplet(massive, {
+      frequency: 400,
+      checkSymmetryFirst: true,
+    });
+    expect(result.js).toHaveLength(0);
+    expect(result.chemShift).toBeCloseTo(7, 0);
+  })
 });
